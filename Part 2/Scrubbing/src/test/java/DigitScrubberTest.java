8b8c54aadb2d94e957e78b8b47c8a7904a7d4c4c
ebaa <ebaaaboelnaga@gmail.com>
2026-04-02 02:50:14 +0200

    refactor and ignore unnecessary files

--- Part 2/Scrubbing/src/test/java/DigitScrubberTest.java
+++ Part 2/Scrubbing/src/test/java/DigitScrubberTest.java
@@ -1,11 +1,10 @@
-import Services.DigitScrubber;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import Services.DigitScrubber;
 
 public class DigitScrubberTest {
 
